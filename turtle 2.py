34258692ec28a8be7aad877a586d7a7a41988827
djhavar <90192071+djhavar@users.noreply.github.com>
2022-08-01 16:45:01 +0530

    Add files via upload

---
+++ turtle 2.py
@@ -0,0 +1,23 @@
+import turtle
+
+turtle.forward(100)
+
+turtle.shape ("circle")
+turtle.right(60)
+turtle.forward(99)
+turtle.shape ("square")
+turtle.right(60)
+turtle.forward(99)
+turtle.shape ("triangle")
+turtle.right(60)
+turtle.forward(99)
+turtle.shape ("circle")
+turtle.right(60)
+turtle.forward(99)
+turtle.shape ("square")
+turtle.right(60)
+turtle.forward(99)
+turtle.shape ("triangle")
+turtle.right(60)
+turtle.forward(99)
+
